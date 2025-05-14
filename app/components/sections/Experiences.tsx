f8b079104455241a498739e3b75dd73d38b2463e
vk211 <vk2111302@gmail.com>
2025-05-14 14:02:12 -0500

    added education and projects

--- app/components/sections/Experiences.tsx
+++ app/components/sections/Experiences.tsx
@@ -2,49 +2,54 @@ import React from "react";
 import { Experience } from "../../../utils/types";
 import { Card } from "../ui/Card";
 export default function Experiences({
-    experiences,
-    sectionId,
-  }: {
-    experiences: Experience[];
-    sectionId: string;
-  }) {
-    return (
-      <section
-        id={sectionId}
-        className="flex justify-center items-center min-h-screen font-mono px-4 sm:px-8 md:px-16 bg-gradient-to-b to-green-50 from-yellow-50 text-gray-900"
-      >
-        <Card>
-          <div className="py-10 px-4 sm:px-6 lg:px-10 max-w-5xl w-full">
-            {experiences.map((exp, index) => (
-              <div
-                key={index}
-                className={`relative pl-10 ${
-                  index === experiences.length - 1
-                    ? "border-l-0 mb-0"
-                    : "border-l-2 border-orange-100 pb-10"
-                }`}
-              >
-                <div className="absolute -left-4 top-1 w-fit h-fit rounded-full text-2xl">💡</div>
-                <div>
-                  <h3 className="text-xl md:text-2xl font-semibold text-green-800">
-                    {exp.role}
-                  </h3>
-                  <p className="text-base md:text-lg text-green-700 flex flex-wrap gap-2 mt-1">
-                    <span className="text-blue-700 font-medium">{exp.company}</span>
-                    <span className="hidden sm:inline">|</span>
-                    <span>{exp.period}</span>
-                  </p>
-                  <ul className="list-disc list-inside mt-3 space-y-2 text-green-900 text-sm md:text-base">
-                    {exp.responsibilities.map((resp, idx) => (
-                      <li key={idx}>{resp}</li>
-                    ))}
-                  </ul>
-                </div>
+  experiences,
+  sectionId,
+}: {
+  experiences: Experience[];
+  sectionId: string;
+}) {
+  return (
+    <section
+      id={sectionId}
+      className="flex flex-col justify-center items-center min-h-screen font-mono px-4 sm:px-8 md:px-16 bg-gradient-to-b to-green-50 from-yellow-50 text-gray-900"
+    >
+      <h1 className="text-4xl font-bold text-center text-gray-700 mb-10">
+     EXPERIENCE
+      </h1>
+
+      <Card>
+        <div className="py-10 px-4 sm:px-6 lg:px-10 max-w-5xl w-full">
+          {experiences.map((exp, index) => (
+            <div
+              key={index}
+              className={`relative pl-10 ${
+                index === experiences.length - 1
+                  ? "border-l-0 mb-0"
+                  : "border-l-2 border-orange-100 pb-10"
+              }`}
+            >
+              <div className="absolute -left-4 top-1 w-fit h-fit rounded-full text-2xl">💡</div>
+              <div>
+                <h3 className="text-xl md:text-2xl font-semibold text-green-800">
+                  {exp.role}
+                </h3>
+                <p className="text-base md:text-lg text-green-700 flex flex-wrap gap-2 mt-1">
+                  <span className="text-blue-700 font-medium">{exp.company}</span>
+                  <span className="hidden sm:inline">|</span>
+                  <span>{exp.period}</span>
+                </p>
+                <ul className="list-disc list-inside mt-3 space-y-2 text-green-900 text-sm md:text-base">
+                  {exp.responsibilities.map((resp, idx) => (
+                    <li key={idx}>{resp}</li>
+                  ))}
+                </ul>
               </div>
-            ))}
-          </div>
-        </Card>
-      </section>
-    );
-  }
+            </div>
+          ))}
+        </div>
+      </Card>
+    </section>
+  );
+}
+
   
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import Experiences from "./components/sections/Experiences";
 import Hero from "./components/sections/Hero";
 import Sidebar from "./components/ui/Sidebar";
 import Skills from "./components/sections/Skills";
-import { aboutMe, experiences, hero, navItems, profile, skill, welcome } from "../utils/data";
+import { aboutMe, experiences, hero, navItems, profile, skill, welcome,projects, educationData } from "../utils/data";
+import Projects from "./components/sections/Projects";
+import Education from "./components/sections/Education";
+
 
 export default function Home() {
   return (
@@ -14,7 +17,9 @@ export default function Home() {
         <About sectionId={navItems[1].id} aboutMe={aboutMe} />
         <Skills sectionId={navItems[2].id} skill={skill} />
         <Experiences sectionId={navItems[3].id} experiences={experiences} />
-        <ContactMe sectionId={navItems[4].id} profile={profile} welcome={welcome.text} />
+        <Projects sectionId={navItems[4].id} projects={projects} />
+        <Education education={educationData} sectionId={navItems[5].id} />      
+        <ContactMe sectionId={navItems[6].id} profile={profile} welcome={welcome.text} />
     </div>
   );
 }
